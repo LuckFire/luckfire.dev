a129267470aa85e8bd61b7d3e04541b63a226651
LuckFire <39076891+LuckFire@users.noreply.github.com>
2025-01-16 20:05:02 -0500

    feat: random 404 messages

--- src/components/pages/NotFound.tsx
+++ src/components/pages/NotFound.tsx
@@ -6,12 +6,18 @@ import { Foreground } from '../Foreground';
 import '../../styling/404.scss';
 
 export class NotFound extends Component {
-    render() { 
+    render() {
+        const errors = [
+            "This page does not exist.",
+            "Whatever you're looking for, it's not here.",
+            "You're like a fish out of water."
+        ];
+
         return <>
             <Foreground>
                 <div class="error">
                     <h1 class="status">404</h1>
-                    <h2 class="info">Wherever you're looking, this ain't it.</h2> 
+                    <h2 class="info">{errors[Math.floor(Math.random() * errors.length)]}</h2>
                 </div>
             </Foreground>
             <Background />
