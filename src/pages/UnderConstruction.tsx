eceb7a92758ff1e3d607d2b59d8552412b475359
LuckFire <39076891+LuckFire@users.noreply.github.com>
2025-01-21 16:18:18 -0500

    chore: change under construction page styling

--- src/pages/UnderConstruction.tsx
+++ src/pages/UnderConstruction.tsx
@@ -9,7 +9,7 @@ export class UnderConstruction extends Component {
     render() {
         return <Foreground>
             <div class="under-construction">
-                <a class={`header ${Fonts.Weights.Montserrat900} ${Fonts.Sizes.Montserrat80px}`}>Under Construction</a>
+                <a class={`header ${Fonts.Weights.Montserrat900} ${Fonts.Sizes.Montserrat48px}`}>Under Construction</a>
                 <a class={`info ${Fonts.Weights.Montserrat500} ${Fonts.Sizes.Montserrat20px}`}>This page is currently being worked on, come back another time!</a>
             </div>
         </Foreground>
