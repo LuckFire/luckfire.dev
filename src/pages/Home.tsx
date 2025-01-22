e54654e9b640ec4c3dee76fcd8aacdd07f8baa47
LuckFire <39076891+LuckFire@users.noreply.github.com>
2025-01-22 14:39:04 -0500

    refactor: future proof for more connections

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -33,27 +33,29 @@ export class Home extends Component {
                     asset: XTwitter,
                     scale: 0.75
                 }
-            }
+            },
         ];
 
         return <Foreground>
-            <div class="signature">
-                <Signature />
-            </div>
-            <div class="about-me">
-                <a class={`${Fonts.Weights.Montserrat900} ${Fonts.Sizes.Montserrat24px}`}>Hey, I'm LuckFire!</a>
-                <a class={`${Fonts.Weights.Montserrat500} ${Fonts.Sizes.Montserrat16px}`}>I'm a UI/UX designer and programmer.</a>
-            </div>
-            <div class="connections">
-                {Connections.map(({ url, newWindow, icon }, i) => (
-                    <a
-                        href={url}
-                        target={newWindow ? '_blank' : '_self'}
-                        draggable={false}
-                    >
-                        <icon.asset scale={icon.scale} />
-                    </a>
-                ))}
+            <div class="home">
+                <div class="signature">
+                    <Signature />
+                </div>
+                <div class="about-me">
+                    <a class={`${Fonts.Weights.Montserrat900} ${Fonts.Sizes.Montserrat24px}`}>Hey, I'm LuckFire!</a>
+                    <a class={`${Fonts.Weights.Montserrat500} ${Fonts.Sizes.Montserrat16px}`}>I'm a UI/UX designer and programmer.</a>
+                </div>
+                <div class="connections">
+                    {Connections.map(({ url, newWindow, icon }, i) => (
+                        <a
+                            href={url}
+                            target={newWindow ? '_blank' : '_self'}
+                            draggable={false}
+                        >
+                            <icon.asset scale={icon.scale} />
+                        </a>
+                    ))}
+                </div>
             </div>
         </Foreground>
     }
