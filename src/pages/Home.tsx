837a40a42a2244abaaac36b8b42bb40bc738788a
LuckFire <39076891+LuckFire@users.noreply.github.com>
2025-01-22 14:31:20 -0500

    refactor: iterate through connections list

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,6 +9,33 @@ import '#/styling/pages/home.scss';
 
 export class Home extends Component {
     render() { 
+        const Connections = [
+            {
+                url: 'discord://-/users/399416615742996480',
+                newWindow: false,
+                icon: {
+                    asset: Discord,
+                    scale: 0.75
+                }
+            },
+            {
+                url: 'https://github.com/luckfire',
+                newWindow: true,
+                icon: {
+                    asset: GitHub,
+                    scale: 0.75
+                }
+            },
+            {
+                url: 'https://twitter.com/luckfiredev',
+                newWindow: true,
+                icon: {
+                    asset: XTwitter,
+                    scale: 0.75
+                }
+            }
+        ];
+
         return <Foreground>
             <div class="signature">
                 <Signature />
@@ -18,15 +45,15 @@ export class Home extends Component {
                 <a class={`${Fonts.Weights.Montserrat500} ${Fonts.Sizes.Montserrat16px}`}>I'm a UI/UX designer and programmer.</a>
             </div>
             <div class="connections">
-                <a href="discord://-/users/399416615742996480" draggable={false}>
-                    <Discord scale={0.75} />
-                </a>
-                <a target="_blank" href="https://github.com/luckfire" draggable={false}>
-                    <GitHub scale={0.75} />
-                </a>
-                <a target="_blank" href="https://twitter.com/luckfiredev" draggable={false}>
-                    <XTwitter scale={0.75} />
-                </a>
+                {Connections.map(({ url, newWindow, icon }, i) => (
+                    <a
+                        href={url}
+                        target={newWindow ? '_blank' : '_self'}
+                        draggable={false}
+                    >
+                        <icon.asset scale={icon.scale} />
+                    </a>
+                ))}
             </div>
         </Foreground>
     }
